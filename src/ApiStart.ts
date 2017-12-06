ed66ac0fc1e023edf7ef4c727f1a4352b8df8c0e
Marcin <cimon87@gmail.com>
2017-12-06 17:18:23 +0100

    forrmat

--- src/ApiStart.ts
+++ src/ApiStart.ts
@@ -1,4 +1,4 @@
-import {AutoWired, Inject, Singleton} from "typescript-ioc";
+import { AutoWired, Inject, Singleton } from "typescript-ioc";
 import { MessagesController } from './Controllers/MessagesController';
 import { BasementSecurity } from './BasementSecurity';
 import { ApiRouter } from './ApiRouter';
@@ -8,17 +8,15 @@ import * as Exec from 'child_process';
 import { Server } from "http";
 import * as nodemon from 'nodemon';
 
-class RestApiBasement
-{
+class RestApiBasement {
     @Inject
-    public Security : BasementSecurity;
+    public Security: BasementSecurity;
 
     private http: Server;
     private port: string | number;
     private app: express.Application;
 
-    constructor()
-    {
+    constructor() {
         this.ListenOnExit();
         this.Security.Run();
 
@@ -28,58 +26,53 @@ class RestApiBasement
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(bodyParser.json());
 
-	this.app.use((req, res, next) => {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		next();
-	});
+        this.app.use((req, res, next) => {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
 
         var router = new ApiRouter();
         router.Route(this.app);
     }
 
-    public Start() : void
-    {
+    public Start(): void {
         this.http = this.app.listen(this.port);
-        
+
 
         console.log('API started on: ' + this.port);
     }
 
-    public ListenOnExit(): void 
-    {
-        nodemon.on('exit',  () => {
+    public ListenOnExit(): void {
+        nodemon.on('exit', () => {
             this.Dispose();
         });
 
-        process.on('SIGINT',  () => {
+        process.on('SIGINT', () => {
             this.Dispose();
         });
 
-        process.on('SIGQUIT',  () => {
+        process.on('SIGQUIT', () => {
             this.Dispose();
         });
 
-        process.on('SIGABRT',  () => {
+        process.on('SIGABRT', () => {
             this.Dispose();
         });
 
-        process.on('SIGTERM',  () => {
+        process.on('SIGTERM', () => {
             this.Dispose();
         });
     }
 
-    public Dispose() : void
-    {
+    public Dispose(): void {
         console.log("Disposing");
 
-        this.http.close(); 
-        this.Security.Dispose(); 
+        this.http.close();
+        this.Security.Dispose();
 
-        Exec.exec('sudo rm /var/run/pigpio.pid', function(error)
-        {    
-            if(error)
-            {
+        Exec.exec('sudo rm /var/run/pigpio.pid', function (error) {
+            if (error) {
                 console.log(error);
             }
 
